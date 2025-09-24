d968206d73556da2860f6d8fec38cf406ee8f47f
Swathikaketi <2300031604@kluniversity.in>
2025-09-24 10:04:08 +0530

    Update HelloController.java

--- src/main/java/klu/HelloController.java
+++ src/main/java/klu/HelloController.java
@@ -2,6 +2,8 @@ package klu;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
 @RequestMapping("/backend")
