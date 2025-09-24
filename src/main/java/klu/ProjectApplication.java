5aa1ffe06594f69cbe33cf9090a1da9d29b715ba
Swathikaketi <2300031604@kluniversity.in>
2025-09-24 10:03:35 +0530

    Update ProjectApplication.java

--- src/main/java/klu/ProjectApplication.java
+++ src/main/java/klu/ProjectApplication.java
@@ -1,22 +1,13 @@
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+package klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner printEndpoints(ApplicationContext ctx) {
-        return args -> {
-            RequestMappingHandlerMapping mapping = ctx.getBean(RequestMappingHandlerMapping.class);
-            mapping.getHandlerMethods().forEach((key, value) -> {
-                System.out.println(key + " : " + value);
-            });
-        };
     }
 }
